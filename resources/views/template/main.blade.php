c541132fc517d35b69a56cf7154684cfc47c774c
relaxsbk.exe <pah4n1989@gmail.com>
2025-01-25 04:50:05 +0800

    Удалён welcome.blade.php Создан свой шаблон

---
+++ resources/views/template/main.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <title>@yield('title')</title>
+</head>
+<body class="d-flex flex-column min-vh-100">
+
+    @include('components.header')
+
+    <main class="flex-grow-1 mt-5 mb-5">
+        @yield('main')
+    </main>
+
+    @include('components.footer')
+
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+</body>
+</html>
