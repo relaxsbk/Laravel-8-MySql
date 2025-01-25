bf2e86ad611d212322426fd0278e8d1f5f4a5c60
relaxsbk.exe <pah4n1989@gmail.com>
2025-01-26 03:56:06 +0800

    Изменена заглушка на https://placehold.co/600x400

--- resources/views/Products.blade.php
+++ resources/views/Products.blade.php
@@ -57,25 +57,25 @@
 
         <div class="d-flex gap-5 justify-content-between flex-wrap">
             <div class="card" style="width: 18rem;">
-                <img src="{{asset('assets/nt.png')}}" class="card-img-top" alt="...">
+                <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">NAME</h5>
                 </div>
             </div>
             <div class="card" style="width: 18rem;">
-                <img src="{{asset('assets/nt.png')}}" class="card-img-top" alt="...">
+                <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">NAME</h5>
                 </div>
             </div>
             <div class="card" style="width: 18rem;">
-                <img src="{{asset('assets/nt.png')}}" class="card-img-top" alt="...">
+                <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">NAME</h5>
                 </div>
             </div>
             <div class="card" style="width: 18rem;">
-                <img src="{{asset('assets/nt.png')}}" class="card-img-top" alt="...">
+                <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">NAME</h5>
                 </div>
